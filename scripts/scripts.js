42a22ed0af7a31c376bff282566c489c33b113d3
Vlad <67093050+Ascilator@users.noreply.github.com>
2020-11-20 11:07:26 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+﻿jQuery(function () {
+
+
+
+    let height_rate = [];
+
+    for (var i = 0; i < $('.rate').length; i++) {
+        height_rate.push($('.rate').eq(i).innerHeight());
+    }
+
+
+    var maxHei = height_rate[0];
+    for (var i = 0; i < height_rate.length; i++) {
+        if (height_rate[i] > maxHei) {
+            maxHei = height_rate[i]
+        }
+    }
+
+    console.log(maxHei)
+    $('.adv_list').css({
+        'margin-top': `${maxHei + 20}px`
+    })
+
+
+});
+
+
